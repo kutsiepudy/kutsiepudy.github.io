6e4d8b32232ad68c6bf6d450f9a24e29a4fa6370
Kristin <kutsiepudy3841@gmail.com>
2026-09-04 13:42:31 -0700

    Add device detection functionality in script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 doomLink = "https://ustymukhman.github.io/WebDOOM/public/"
 
 console.log("STOP PEAKING!")
+
+const detectDevice = () {
+
+  const isMobileScreen = window.watchMedia("(max-width: 1024px)").matches;
+  const isTouchScreen = window.watchMedia("(pointer: coarse)").matches || ('ontouchstart' in window);
+
+  if (isMobileScreen && isTouchScreen) {
+    return "Are you on Mobile?"
+  }
+  return "Are you on PC?"
+}
+
+console.log(detectDevice());
+
+console.log("well you found the DOOM game")
