b832c8c3b10f7fb909732f43834f2ea0eb177a39
Kristin <kutsiepudy3841@gmail.com>
2026-09-19 11:44:54 -0700

    Center cursor on mouse move

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ const click = "cursor/explosion.gif"
 let isHoveringLink = false;
 
 document.addEventListener('mousemove', (e) => {
-  cursor.style.transform = `translate3d(${e.clientX}px, ${e.clientY}px, 0)`;
+  cursor.style.transform = `translate3d(${e.clientX}px, ${e.clientY}px, 0) translate(-50%, -50%)`;
   if (!isHoveringLink && !document.querySelector(".cursor-container.is-clicking")) {
     cursorAsset.src = idle;
   }
